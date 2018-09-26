d33ec84211d30b8aac010ac2d2fc091f759b3693
ZePedroResende <j.p.resende68@gmail.com>
2018-09-26 05:31:27 +0100

    Fix home and details CSS and structure

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -8,6 +8,7 @@ export class DetailsPage {
     item: any;
     date1: any;
     colors: any;
+    anexo: any;
 
     constructor(public navCtrl: NavController, public params: NavParams) {
         this.colors={
@@ -16,6 +17,17 @@ export class DetailsPage {
         };
         this.item = params.get('item');
         this.date1 = new Date(Date.parse(this.item.validade[1]));
+
+        this.anexo = this.item.anexos[0];
     }
+
+
+    download(){
+    this.http.get(this.anexo, {responseType: 'blob'})
+    .subscribe((imageBlob: Blob) => {
+        return this.file.writeFile(cordova.file.externalRootDirectory + '/Download/', "anexo", imageBlob, {replace: true});
+    }).toPromise();
+}
+
 
 }
